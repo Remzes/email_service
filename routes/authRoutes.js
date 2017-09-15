7fa8d09cd58cf7b6e56d42359c704af0603b7e0c
Remzes <victor_s1998@mail.ru>
2017-09-15 08:05:22 +0300

    Sendgrid optimized

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,12 +14,6 @@ module.exports = (app) => {
         }
     );
 
-    app.get('/', (req, res) => {
-        if (req.user) {
-            res.redirect('/surveys');
-        }
-    });
-
     app.get('/api/logout', (req, res) => {
        req.logout();
        res.redirect('/');
